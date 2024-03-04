567f8c729b6a00d62f3668ff350e604b5f254d79
UmarMsani <umuhammad220@gmail.com>
2024-03-04 18:56:37 +0000

    Implement createIteratorObject function

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,17 +2,16 @@ export default function createIteratorObject(report) {
   const departments = Object.values(report.allEmployees);
   const flattenedEmployees = departments.flat();
 
-  report[Symbol.iterator] = function () {
-    let currentIndex = 0;
+  let currentIndex = 0;
 
-    return {
-      next() {
-        return currentIndex < flattenedEmployees.length
-          ? { value: flattenedEmployees[currentIndex++], done: false }
-          : { done: true };
-      },
-    };
+  return {
+    next() {
+      return currentIndex < flattenedEmployees.length
+        ? { value: flattenedEmployees[currentIndex++], done: false }
+        : { done: true };
+    },
+    [Symbol.iterator]() {
+      return this;
+    },
   };
-
-  return report;
 }
