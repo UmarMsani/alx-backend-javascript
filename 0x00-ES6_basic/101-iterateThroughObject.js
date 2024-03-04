ccb342c569451c19192860eb6cd0951876d2d165
UmarMsani <umuhammad220@gmail.com>
2024-03-04 19:01:33 +0000

    Implement iterateThroughObject function

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,13 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+
+  for (const employee of reportWithIterator) {
+    result += employee + ' | ';
+  }
+
+  if (result.length > 0) {
+    result = result.slice(0, -3);
+  }
+
+  return result;
+}
