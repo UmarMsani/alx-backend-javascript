ef7d134605cf50403500e9e7e208956bad6b3cdd
UmarMsani <umuhammad220@gmail.com>
2024-03-08 17:43:27 +0000

    Implement Pricing class with Currency integration

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,107 +1,59 @@
-import Currency from './3-currency';
 /* eslint-disable no-underscore-dangle */
+import Currency from './3-currency';
 
-/**
- * Class representing a Pricing with an amount and currency.
- * @class
- */
 export default class Pricing {
   /**
    * Create a new Pricing instance.
    *
-   * @param {Number} amount - The amount of the pricing.
-   * @param {Currency} currency - The currency of the pricing.
+   * @param {number} amount - The amount of money.
+   * @param {Currency} currency - The currency.
    */
   constructor(amount, currency) {
-    // Validate and set the amount attribute
-    this._amount = this.validateNumber(amount, 'amount');
-
-    // Validate and set the currency attribute
-    this._currency = this.validateCurrency(currency, 'currency');
+    this.amount = amount;
+    this.currency = currency;
   }
 
-  /**
-   * Get the amount of the pricing.
-   * @returns {Number} - The amount of the pricing.
-   */
   get amount() {
     return this._amount;
   }
 
-  /**
-   * Set the amount of the pricing.
-   *
-   * @param {Number} value - The new amount for the pricing.
-   * @throws {TypeError} Will throw an error if the value is not a number.
-   */
   set amount(value) {
-    this._amount = this.validateNumber(value, 'amount');
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
   }
 
-  /**
-   * Get the currency of the pricing.
-   * @returns {Currency} - The currency of the pricing.
-   */
   get currency() {
     return this._currency;
   }
 
-  /**
-   * Set the currency of the pricing.
-   *
-   * @param {Currency} value - The new currency for the pricing.
-   * @throws {TypeError} Will throw an error if the value is not a valid Currency instance.
-   */
   set currency(value) {
-    this._currency = this.validateCurrency(value, 'currency');
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = value;
   }
 
   /**
    * Display the full pricing information.
-   * @returns {String} - Formatted string containing amount and currency information.
+   * @returns {string} - Formatted string containing amount and currency information.
    */
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-  }
-
-  /**
-   * Validate that the value is a number.
-   *
-   * @param {any} value - The value to validate.
-   * @param {String} attribute - The name of the attribute.
-   * @returns {Number} - The validated number.
-   * @throws {TypeError} Will throw an error if the value is not a number.
-   */
-  validateNumber(value, attribute) {
-    if (typeof value !== 'number' || isNaN(value)) {
-      throw new TypeError(`${attribute} must be a valid number`);
-    }
-    return value;
-  }
-
-  /**
-   * Validate that the value is a valid Currency instance.
-   *
-   * @param {any} value - The value to validate.
-   * @param {String} attribute - The name of the attribute.
-   * @returns {Currency} - The validated Currency instance.
-   * @throws {TypeError} Will throw an error if the value is not a valid Currency instance.
-   */
-  validateCurrency(value, attribute) {
-    if (!(value instanceof Currency)) {
-      throw new TypeError(`${attribute} must be an instance of Currency`);
-    }
-    return value;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   /**
    * Static method to convert a price using a conversion rate.
    *
-   * @param {Number} amount - The amount to convert.
-   * @param {Number} conversionRate - The conversion rate.
-   * @returns {Number} - The converted amount.
+   * @param {number} amount - The amount to convert.
+   * @param {number} conversionRate - The conversion rate.
+   * @returns {number} - The converted amount.
    */
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversionRate must be numbers');
+    }
     return amount * conversionRate;
   }
 }
