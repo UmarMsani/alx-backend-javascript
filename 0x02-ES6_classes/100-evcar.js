86d5cd19a6a8a286bfe3e1dc39c9180bf9589b84
UmarMsani <umuhammad220@gmail.com>
2024-03-08 19:47:24 +0000

    Implement EVCar class extending Car

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-underscore-dangle */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  /**
+   * Clone the car.
+   * @returns {EVCar} - A new instance of EVCar.
+   */
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+    return new Species();
+  }
+}
