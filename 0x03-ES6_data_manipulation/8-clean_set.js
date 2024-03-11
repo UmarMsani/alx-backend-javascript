1f2452bd53aa78ca64f58941e786c192fb657a0f
UmarMsani <umuhammad220@gmail.com>
2024-03-11 12:22:10 +0000

    CleanSet function for clarity

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+const cleanSet = (set, startString) => {
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+
+  const strings = [];
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      strings.push(s.slice(startString.length));
+    }
+  });
+
+  return strings.join('-');
+};
+
+export default cleanSet;
