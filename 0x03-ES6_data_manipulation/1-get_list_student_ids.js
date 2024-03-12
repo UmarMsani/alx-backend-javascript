0e067ae0c5a44a11200d72bf50b2915758f5a94f
UmarMsani <umuhammad220@gmail.com>
2024-03-12 10:39:42 +0000

    update

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,19 @@
-function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
+/**
+ * Returns an array of student IDs from an array of student objects.
+ * @param {Array} arr - The input array containing student objects.
+ * @returns {Array} - An array of student IDs or an empty array if the input is not an array.
+ */
+export default function getListStudentIds(arr) {
+  // Key to access in each element of the array
+  const keyToAccess = 'id';
+
+  // Check if the input is an array
+  if (Array.isArray(arr)) {
+    // Map each element to its 'id' property and return the resulting array
+    const valuesArray = arr.map((ele) => ele[keyToAccess]);
+    return valuesArray;
   }
 
-  return students.map(student => student.id);
+  // Return an empty array if the input is not an array
+  return [];
 }
-
-export default getListStudentIds;
