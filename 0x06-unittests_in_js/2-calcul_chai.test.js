d3bfb53287f70ca6582cf3819ad4527af5f5e3bd
UmarMsani <umuhammad220@gmail.com>
2024-05-02 14:14:07 +0000

    Unittests in JS

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', () => {
+    describe('SUM', () => {
+        it('should return the sum of rounded numbers', () => {
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+        });
+    });
+
+    describe('SUBTRACT', () => {
+        it('should return the difference of rounded numbers', () => {
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+        });
+    });
+
+    describe('DIVIDE', () => {
+        it('should return the division result of rounded numbers', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+        });
+
+        it('should return "Error" when dividing by zero', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        });
+    });
+});
